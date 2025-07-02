e4a0b2827126993a40c0a81907a7b83efc1ec423
AndresCruz1980 <andrescruz.1980@gmail.com>
2025-07-02 11:35:33 -0400

    Add miembros table migration and model setup

--- app/Models/Miembro.php
+++ app/Models/Miembro.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Miembro extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'licencia',
+        'numero',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
